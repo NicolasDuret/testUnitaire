b75ffd59253ffac8155c0e3a8d022e540a515669
Nicolas Duret <nicolas@avecnous.eu>
2021-03-30 13:32:51 +0200

    add correction

--- api/services/CalculService.js
+++ api/services/CalculService.js
@@ -1,26 +1,38 @@
+const sinon  = require("sinon")
+
 module.exports = {
 
-    add : (a , b  ) => {
-        return a+b;
-    },
 
-    sub : (a , b ) => {
-        return a -b;
-    },
-
-    mul : (a , b  ) => {
-        return a*b;
-    },
-
-    div : (a , b  ) => {
-        return a/b;
-    },
-
-    prct : (a , prct  ) => {
-      let result = CalculService.mul(a, prct);
-      result = CalculService.div(result , 100)
-      return result;
+  add : (a , b) => {
+    return a + b;
   },
+
+  sub : (a , b ) =>{
+    return a - b;
+  },
+
+  mul : (a , b ) =>{
+    return a * b;
+  },
+
+  div : (a , b ) =>{
+    if (b === 0){
+      throw new Error("Divide by zero not allowed");
+    }
+    return a / b;
+  },
+
+  prct : (a , prct) => {
+    let result = CalculService.mul(a , prct);
+    result = CalculService.div(result , 100);
+    return result;
+  },
+
+  pow : (a) =>{
+    return CalculService.mul(a , a);
+  }
+
+
 
 
 
--- test/fixture/services/CalculService.test.js
+++ test/fixture/services/CalculService.test.js
@@ -1,55 +1,104 @@
 const assert = require('assert');
+const sinon = require('sinon');
 
-describe('CalculService' , function (){
-  describe('#add', function (){
+describe('CalculService' , function(){
 
-    it('should add values' , function (){
-      let result = CalculService.add(1,5);
+  describe('#add' , function(){
 
-      assert.equal(result, 6);
-
+    it('should add values' , function(){
+      let result = CalculService.add( 1 , 5);
+      assert.equal(result , 6);
     })
-  })
-  describe('#sub', function (){
-    it('should sub values' , function (){
-      let result = CalculService.sub(5,4);
 
-      assert.equal(result, 1);
 
-    })
-  })
-  describe('#mul', function (){
-    it('should mul values' , function (){
-      let result = CalculService.mul(1,5);
-
-      assert.equal(result, 5);
-
-    })
-  })
-  describe('#div', function (){
-    it('should div values' , function (){
-      let result = CalculService.div(5,5);
-
-      assert.equal(result, 1);
-
-    })
   })
 
-  describe('#prct', function (){
-    it('should calculate prct values' , function (){
+  describe('#sub' , function(){
+    it('should sub values' , function(){
+      let result = CalculService.sub( 3 , 2);
+      assert.equal(result , 1);
+    })
+
+  })
+
+  describe('#mul' , function(){
+    it('should mul values' , function(){
+      let result = CalculService.mul( 2 , 5);
+      assert.equal(result , 10);
+    })
+
+  })
+
+  describe('#div' , function(){
+    it('should div values' , function(){
+      let result = CalculService.div( 25 , 5);
+      assert.equal(result , 5);
+    })
+
+    it('should not div value by 0' , function(){
+      try {
+        let result = CalculService.div(25, 0);
+      }catch(e){
+        assert(e.message , 'Divide by zero not allowed');
+        return;
+      }
+      assert(false);
+    })
+
+  })
+
+  describe('#prct' , function(){
+    it('should calculate a percentage' , function(){
       var stub1 = sinon.stub(CalculService, 'mul').callsFake((a , b) => {return 5})
       var stub2 = sinon.stub(CalculService, 'div').callsFake((a , b) => {return 20})
-      let result = CalculService.prct(100,25);
 
-      assert.equal(result, 1);
+      let result = CalculService.prct( 100 , 25);
+
+
+
+      assert.equal(result , 20);
 
       CalculService.mul.restore();
+      CalculService.div.restore();
+
 
     })
-    it('should calls div and mul when calculating a percentage' , function (){
-      const spyful = sinon.spy(CalculService, 'mul');
+
+    it('should calls div and mul when calculating a percentage' , function(){
+      const spyMul = sinon.spy(CalculService, "mul");
+      const spyDiv = sinon.spy(CalculService, "div");
+      const result = CalculService.prct(10 , 10);
+
+      assert(spyDiv.calledOnce);
+      assert(spyMul.calledOnce);
+
+      assert(spyDiv.calledImmediatelyAfter(spyMul));
+
+
+      CalculService.mul.restore();
+      CalculService.div.restore();
 
     })
+
   })
 
+
+  describe('#pow' , function(){
+
+    it('should calls div and mul when calculating a percentage' , function(){
+      const spyMul = sinon.spy(CalculService, "mul");
+      const spyDiv = sinon.spy(CalculService, "div");
+      const result = CalculService.pow(5);
+
+      assert.equal(spyDiv.callCount , 0);
+      assert(spyMul.calledOnce);
+      assert(spyMul.calledWith(5 , 5));
+
+
+      CalculService.mul.restore();
+      CalculService.div.restore();
+
+    })
+
+  })
 })
